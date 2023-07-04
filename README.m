d86aa37204e4341864c457fb27b71c07480fc624
AnalystVinayak <138573437+AnalystVinayak@users.noreply.github.com>
2023-07-04 14:37:53 +0530

    Create README.m

---
+++ README.m
@@ -0,0 +1,10 @@
+- 👋 Hi, I’m @AnalystVinayak
+- 👀 I’m interested in ... Machine Learning and Automation
+- 🌱 I’m currently learning ... Data Analysis
+- 💞️ I’m looking to collaborate on ...Automation of HR and other Operational functions 
+- 📫 How to reach me ...vinayak.mba49@gmail.com
+
+<!---
+AnalystVinayak/AnalystVinayak is a ✨ special ✨ repository because its `README.md` (this file) appears on your GitHub profile.
+You can click the Preview link to take a look at your changes.
+--->
